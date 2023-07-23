7fccbc4c6f640c6c7ff6a710f97a1c31468dc30c
cocoson <cocoson23@outlook.com>
2023-07-23 14:22:31 +0800

    Update driver code

--- Code/LinuxDevelopment/LED/led.c
+++ Code/LinuxDevelopment/LED/led.c
@@ -1,24 +1,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+#define LEDON _IO('L', 1)
+#define LEDOFF _IO('L' ,0)
+
+int main(int argc, char* argv[])
 {
+	if(argc < 2) {
+		printf("error command, usage: ./led ledx\n");
+		return -1;
+	}
+	// 打开字符设备 => 调用对应的led_open()函数
+    int fd = open(argv[1],O_RDWR);
+	if(-1 == fd) {
+        perror("Open LED");
+        return -1;
+    }
+
     while(1) {
-		// 打开字符设备 => 调用对应的led_open()函数
-        int fd = open("led",O_RDWR);
+		ioctl(fd, LEDON, 0);
 		sleep(1);
-		if(-1 == fd) {
-                	perror("Open LED");
-                	return -1;
-        }
-        // 关闭字符设备 => 调用led_off()函数
-		close(fd);
+		ioctl(fd, LEDOFF, 0);
+		sleep(1);
+		ioctl(fd, LEDON, 1);
+		sleep(1);
+		ioctl(fd, LEDOFF, 1);
 		sleep(1);
     }
+
+    // 关闭字符设备 => 调用led_off()函数
+	close(fd);
+
     return 0;
 }
 
--- Code/LinuxDevelopment/LED/led_driver.c
+++ Code/LinuxDevelopment/LED/led_driver.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/version.h>
+#include <linux/ioctl.h>
 #include <asm/io.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
@@ -10,58 +11,129 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("cocoson");
 MODULE_DESCRIPTION("LED Driver");
 
-unsigned int *CLK_PCLKEN0;
-unsigned int *GPIOPB_DIR;
-unsigned int *GPIOPB_DATAOUT;
+#define LEDON _IO('L', 1)
+#define LEDOFF _IO('L' ,0)
+
+struct {  
+	// 创建cdev结构体
+	struct cdev *cdev;
+	dev_t devnum;
+	unsigned int *CLK_PCLKEN0;
+	unsigned int *GPIOPB_DIR;
+	unsigned int *GPIOPB_DATAOUT;
+} led;
 
 // LED0 LED1 ON
-int led_on(struct inode *i, struct file *f)
+int led_on(int i)
 {
-	*GPIOPB_DATAOUT &= 0xfffffffC;
-	printk("LED ON\n");
+	switch (i)
+	{
+	case 0:
+		*led.GPIOPB_DATAOUT &= 0xfffffff2;
+		printk("LED0 ON\n");
+		break;
+	case 1:
+		*led.GPIOPB_DATAOUT &= 0xfffffff1;
+		printk("LED1 ON\n");
+		break;
+	default:
+		break;
+	}
+	
 	return 0;
 }
 
 // LED0 LED1 OFF
-int led_off(struct inode *i, struct file *f)
+int led_off(int i)
 {
-	*GPIOPB_DATAOUT |= 0x00000003;
-	printk("LED OFF\n");
+	switch (i)
+	{
+	case 0:
+		*led.GPIOPB_DATAOUT |= 0x00000001;
+		printk("LED0 OFF\n");
+		break;
+	case 1:
+		*led.GPIOPB_DATAOUT |= 0x00000002;
+		printk("LED1 OFF\n");
+		break;
+	default:
+		break;
+	}
+	
+	return 0;
+}
+
+int led_init(struct inode *i, struct file *f) 
+{
+	//CLK_PCLKEN0 = ioremap(0xB0000218, 4);
+	led.GPIOPB_DIR = ioremap(0xB8003040, 4);
+	led.GPIOPB_DATAOUT = ioremap(0xB8003044, 4);
+	//*CLK_PCLKEN0 = 0x00000008;
+	*led.GPIOPB_DIR |= 0x00000003;
+	// 灯初始化熄灭
+	*led.GPIOPB_DATAOUT |= 0x00000003;
+	printk("Register init finished\n");
+
+	return 0;
+}
+
+int led_release(struct inode *i, struct file *f)
+{
+	*led.GPIOPB_DATAOUT |= 0x00000003;
+
+	// 解除内存映射
+    //iounmap(led.CLK_PCLKEN0);
+    iounmap(led.GPIOPB_DIR);
+    iounmap(led.GPIOPB_DATAOUT);
+
+	return 0;
+}
+
+long led_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+{
+	switch (cmd)
+	{
+	case LEDON:
+		printk("cmd: %d\n", LEDON);
+		led_on(arg);
+		break;
+	case LEDOFF:
+		printk("cmd: %d\n", LEDOFF);
+		led_off(arg);
+		break;
+	default:
+		break;
+	};
+
 	return 0;
 }
 
 static struct file_operations ledops = {
-  .open = led_on,
-  .release = led_off,
+  // 专门挂接设备初始化动作
+  .open = led_init,
+  // 专门挂接设备释放动作
+  .release = led_release,
+  // 挂接设备控制函数
+  .unlocked_ioctl = led_ioctl,
 }; 
 
-// 创建cdev结构体
-struct cdev *mycdev;
-	
-dev_t devnum;
+
 
 // 插入模块时调用该函数
-static int __init led_init(void)
+static int __init led_module_init(void)
 {
-	//CLK_PCLKEN0 = ioremap(0xB0000218, 4);
-	GPIOPB_DIR = ioremap(0xB8003040, 4);
-	GPIOPB_DATAOUT = ioremap(0xB8003044, 4);
-	//*CLK_PCLKEN0 = 0x00000008;
-	*GPIOPB_DIR |= 0x00000003;
-	printk("Register init finished\n");
-	
 	// 申请主次设备号
-	alloc_chrdev_region(&devnum, 0, 1, "LED");	
-	printk("devnum: %d\n", MAJOR(devnum));
+	alloc_chrdev_region(&led.devnum, 0, 1, "LED");	
+	printk("devnum: %d\n", MAJOR(led.devnum));
 
 	// 构造cdev结构体 (char device struct)
-	mycdev = cdev_alloc();
+	led.cdev = cdev_alloc();
 
 	// 使用ops结构体初始化dev结构体
-	cdev_init(mycdev, &ledops);
+	cdev_init(led.cdev, &ledops);
 	
 	//注册到字符设备驱动框架中
-	cdev_add(mycdev, devnum, 1);
+	cdev_add(led.cdev, led.devnum, 1);
 
 	printk("driver add success\n");
 	printk("Insert my led driver\n");
@@ -70,24 +142,20 @@ static int __init led_init(void)
 
 // 模块析构函数
 // 卸载模块时调用该函数
-static void __exit led_exit(void)
+static void __exit led_module_exit(void)
 {
+
 	printk("led driver delete start\n");
 	// 从字符设备驱动框架中注销驱动
-	cdev_del(mycdev);
+	cdev_del(led.cdev);
 	// 注销设备号
-	unregister_chrdev_region(devnum, 1);
-
-	// 解除内存映射
-        iounmap(CLK_PCLKEN0);
-        iounmap(GPIOPB_DIR);
-        iounmap(GPIOPB_DATAOUT);
+	unregister_chrdev_region(led.devnum, 1);
 	
 	printk("my led driver deleted\n");
 }
 
-// 将函数hello_init设置为模块加载函数
-module_init(led_init);
-// 将函数hello_exit设置为模块删除函数
-module_exit(led_exit);
+// 将函数module_init设置为模块加载函数
+module_init(led_module_init);
+// 将函数module_exit设置为模块删除函数
+module_exit(led_module_exit);
 
